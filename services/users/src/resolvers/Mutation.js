407e47d6b89c6e49b548d7151fa81908a1c470a2
Sarah Gujadhur <sarahgujadhur@gmail.com>
2018-10-04 11:48:54 -0400

    played with mutation and query in prisma

--- services/users/src/resolvers/Mutation.js
+++ services/users/src/resolvers/Mutation.js
@@ -35,13 +35,243 @@ async function login (parent, args, context, info) {
   }
 }
 
-// async function editUser (parent, args, context, info) {
-//   const userId = getUserId(context);
-
-
-// }
+const Mutation = {
+  editUser: (parent, args, context, info) => {
+    const userId = getUserId(context);
+    return context.db.mutation.updateUser(
+      {
+        data: {
+          user_name: args.user_name,
+          first_name: args.first_name,
+          last_name: args.last_name,
+          email: args.email,
+          phone_number: args.phone_number,
+          password: args.password
+        },
+        where: {id: userId}
+      },
+      info
+    )
+  },
+  addLocation: (parent, args, context, info) => {
+    const userId = getUserId(context);
+    return context.db.mutation.createLocation({
+        data: {   
+          user_id: userId,
+          name: args.name,
+          street1: args.street1,
+          street2: args.street2,
+          city: args.city,
+          state: args.state,
+          zip: args.zip
+        }
+      },
+      info
+    )
+  },
+  editLocation: (parent, args, context, info) => {
+    const userId = getUserId(context);
+    return context.db.mutation.updateLocation(
+      {
+        data: {
+          name: args.name,
+          street1: args.street1,
+          street2: args.street2,
+          city: args.city,
+          state: args.state,
+          zip: args.zip
+        },
+        where: {id: userId}
+      },
+      info
+    )
+  }
+  // deleteLocation: (parent, args, context, info) => {
+  //   type: LocationType,
+  //   args: {
+  //     id: {type: new GraphQLNonNull(GraphQLID)},
+  //     user_id: {type: new GraphQLNonNull(GraphQLID)}
+  //   },
+  //   resolve(parent, args, context){
+  //     const userId = getUserId(context);
+  //     return Location.destroy({where: {id: args.id}})
+  //       .then(() => {
+  //         return Location.findAll({where: {user_id: args.user_id}});
+  //       })
+  //       .catch((err) => {
+  //         console.log('Error caught on deleteLocation in mutation.js', err);
+  //       });
+  //   }
+  // },
+  // addCar: (parent, args, context, info) => {
+  //   type: CarType,
+  //   args: {
+  //     size: {type: new GraphQLNonNull(GraphQLInt)},
+  //     make: {type: new GraphQLNonNull(GraphQLString)},
+  //     model: {type: GraphQLString},
+  //     color: {type: new GraphQLNonNull(GraphQLString)},
+  //     plate: {type: GraphQLString},
+  //     state: {type: GraphQLString}
+  //   },
+  //   resolve(parent, args, context) {
+  //     const userId = getUserId(context);
+  //     return Car.create({
+  //       size: args.size,
+  //       make: args.make,
+  //       model: args.model,
+  //       color: args.color,
+  //       plate: args.plate,
+  //       state: args.state,
+  //       user_id: userId 
+  //     })
+  //       .catch((err) => {
+  //         console.log('Error caught on addCar in mutation.js', err);
+  //       });
+  //   }
+  // },
+  // editCar: (parent, args, context, info) => {
+  //   type: CarType,
+  //   args: {
+  //     id: {type: new GraphQLNonNull(GraphQLID)},
+  //     size: {type: GraphQLInt},
+  //     make: {type: GraphQLString},
+  //     model: {type: GraphQLString},
+  //     color: {type: GraphQLString},
+  //     plate: {type: GraphQLString},
+  //     state: {type: GraphQLString},
+  //   },
+  //   resolve(parent, args, context) {
+  //     const userId = getUserId(context);
+  //     return Car.update({
+  //       size: args.size,
+  //       make: args.make,
+  //       model: args.model,
+  //       color: args.model,
+  //       plate: args.plate,
+  //       state: args.state,
+  //     }, {
+  //       where: {id: args.id}
+  //     })
+  //       .then(() => {
+  //         return Car.find({where: {id: args.id}});
+  //       })
+  //       .catch((err) => {
+  //         console.log('Error caught on editCar in mutation.js', err);
+  //       });
+  //   }
+  // },
+  // deleteCar: (parent, args, context, info) => {
+  //   type: CarType,
+  //   args: {
+  //     id: {type: new GraphQLNonNull(GraphQLID)}
+  //   },
+  //   resolve(parent, args, context) {
+  //     const userId = getUserId(context);
+  //     return Car.destroy({where: {id: args.id}})
+  //       .then(() => {
+  //         return Car.findAll({where: {user_id: userId}});
+  //       })
+  //       .catch((err) => {
+  //         console.log('Error caught on deleteCar in mutation.js', err);
+  //       });
+  //   }
+  // },
+  // //add listing, edit listing
+  // addSpot: (parent, args, context, info) => {
+  //   type: SpotType,
+  //   args: {
+  //     user_id: {type: new GraphQLNonNull(GraphQLID)},
+  //     lat: {type:GraphQLString},
+  //     lng: {type:GraphQLString},
+  //     street1: {type:GraphQLString},
+  //     street2: {type:GraphQLString},
+  //     city: {type:GraphQLString},
+  //     state: {type:GraphQLString},
+  //     zip: {type:GraphQLInt},
+  //     type: {type: new GraphQLNonNull(GraphQLString)},
+  //     start_time: {type: new GraphQLNonNull(resolverMap.Date)},
+  //     end_time: {type:resolverMap.Date}
+  //   },
+  //   resolve(parent, args) {
+  //     return Spot.create({
+  //       user_id: args.user_id,
+  //       lat: args.lat,
+  //       lng: args.lng,
+  //       street1: args.street1,
+  //       street2: args.street2,
+  //       city: args.city,
+  //       state: args.state,
+  //       zip: args.zip,
+  //       is_available: true,
+  //       type: args.type,
+  //       start_time: args.start_time,
+  //       end_time: args.end_time
+  //     })
+  //       .catch((err) => {
+  //         console.log('Error caught on addSpot in mutation.js', err);
+  //       });
+  //   }
+  // },
+  // editSpot: (parent, args, context, info) => {
+  //   type: SpotType,
+  //   args: {
+  //     id: {type: new GraphQLNonNull(GraphQLID)},
+  //     user_id: {type: GraphQLID},
+  //     lat: {type:GraphQLString},
+  //     lng: {type:GraphQLString},
+  //     street1: {type:GraphQLString},
+  //     street2: {type:GraphQLString},
+  //     city: {type:GraphQLString},
+  //     state: {type:GraphQLString},
+  //     zip: {type:GraphQLInt},
+  //     type: {type: GraphQLString},
+  //     start_time: {type: GraphQLString},
+  //     end_time: {type:GraphQLString}
+  //   },
+  //   resolve(parent, args) {
+  //     return Spot.update({
+  //       user_id: args.user_id,
+  //       lat: args.lat,
+  //       lng: args.lng,
+  //       street1: args.street1,
+  //       street2: args.street2,
+  //       city: args.city,
+  //       state: args.state,
+  //       zip: args.zip,
+  //       is_available: true,
+  //       type: args.type,
+  //       start_time: args.start_time,
+  //       end_time: args.end_time
+  //     }, {
+  //       where: {id: args.id}
+  //     })
+  //       .then(() => {
+  //         return Spot.find({where: {id: args.id}});
+  //       })
+  //       .catch((err) => {
+  //         console.log('Error caught on editSpot in mutation.js', err);
+  //       });
+  //   }
+  // },
+  // deleteSpot: (parent, args, context, info) => {
+  //   type: SpotType,
+  //   args: {
+  //     id: {type: new GraphQLNonNull(GraphQLID)}
+  //   },
+  //   resolve(parent, args) {
+  //     return Spot.destroy({where: {id: args.id}})
+  //       .then(() => {
+  //         console.log('Success');
+  //       })
+  //       .catch((err) => {
+  //         console.log('Error caught on deleteSpot in mutation.js', err);
+  //       });
+  //   }
+  // },
+}
 
 module.exports = {
   signup,
   login,
+  Mutation
 }
--- services/users/src/resolvers/Query.js
+++ services/users/src/resolvers/Query.js
@@ -1,3 +1,49 @@
+// const Query = {
+//   user: (parent, args, context, info) => {
+//     if (!args.id && !args.email) {
+//       return context.db.query.user({ where: {user_name: args.user_name} }, info);
+//     }
+//     else if (!args.user_name && !args.id) {
+//       return context.db.query.user({ where: {email: args.email} }, info);
+//     }
+//     else {
+//       return context.db.query.user({ where: {id: args.id} }, info);
+//     }
+//   },
+//   users: (parent, args, context, info) => {
+//     return context.db.query.users({ }, info);
+//   },
+//   location: (parent, args, context, info) => {
+//     return context.db.query.location({ where: {id: args.id} }, info);
+//   },
+//   locations: (parent, args, context, info) => {
+//     return context.db.query.locations({ where: {user_id: args.user_id} }, info);
+//   },
+//   car: (parent, args, context, info) => {
+//     return context.db.query.car({ where: {id: args.id} }, info);
+//   },
+//   cars: (parent, args, context, info) => {
+//     return context.db.query.cars({ where: {user_id: args.user_id} }, info);
+//   },
+//   spot: (parent, args, context, info) => {
+//     return context.db.query.spot({ where: {id: args.id} }, info);
+//   },
+//   spots: (parent, args, context, info) => {
+//     return context.db.query.spots({ }, info);
+//   },
+//   listing: (parent, args, context, info) => {
+//     return context.db.query.listing({ where: {id: args.id} }, info);
+//   },
+//   listings: (parent, args, context, info) => {
+//     return context.db.query.listings({ }, info);
+//   },
+//   openListings: (parent, args, context, info) => {
+//     return context.db.query.listings({where: {status: 1} }, info);
+//   }
+// };
+
+
+
 function user (parent, args, context, info) {
   if (!args.id && !args.email) {
     return context.db.query.user({ where: {user_name: args.user_name} }, info);
