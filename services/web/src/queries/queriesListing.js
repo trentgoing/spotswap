d0124b4de1486dfd4c005d7e4ba7934af99e4efa
Sarah Gujadhur <sarahgujadhur@gmail.com>
2018-10-06 16:43:46 -0400

    created two new folders

--- services/web/src/queries/queriesListing.js
+++ services/web/src/queries/queriesListing.js
@@ -2,11 +2,13 @@ import { gql } from 'apollo-boost';
 
 const editListingMutation = gql`
 mutation(
+  $id: ID
   $claiming_user_id: ID,
   $status: Int,
   $time_complete: DateTime
 ) {
   editListing(
+    id: $id
     claiming_user_id: $claiming_user_id
     status: $status
     time_complete: $time_complete
