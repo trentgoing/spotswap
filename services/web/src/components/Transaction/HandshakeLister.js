899438e7d034c27e762f9f7812e1cbc6f47c9302
Trent Going <trent.going@gmail.com>
2018-10-09 19:01:28 -0400

    Color background for all users who have a transaction in process

--- services/web/src/components/Transaction/HandshakeLister.js
+++ services/web/src/components/Transaction/HandshakeLister.js
@@ -28,6 +28,22 @@ const CHANGED_LISTINGS_SUBSCRIPTION = gql`
   }
 `;
 
+function hashCode(str) { // java String#hashCode
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+     hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  return hash;
+} 
+
+function intToRGB(i){
+  var c = (i & 0x00FFFFFF)
+      .toString(16)
+      .toUpperCase();
+
+  return "00000".substring(0, 6 - c.length) + c;
+}
+
 class HandshakeLister extends Component {
   constructor(props) {
     super(props);
@@ -85,7 +101,8 @@ class HandshakeLister extends Component {
       )
     } else if (listing.status === 2) {
       return (
-        <div key={listing.id}>
+        <div key={listing.id} style={{backgroundColor: '#' + intToRGB(hashCode(listing.spot.id))}}>
+          {intToRGB(hashCode(listing.id))}
           CLAIMED
           <p>Your spot has been claimed</p>
           {JSON.stringify(listing)}
